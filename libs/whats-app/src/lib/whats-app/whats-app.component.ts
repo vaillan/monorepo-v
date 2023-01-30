5bfa88d1cd2127640813dabbf35d20b3437dd2b1
valentin-ortiz <vortiz@capi.lat>
2023-01-30 11:59:24 -0600

    Whats app UI

--- libs/whats-app/src/lib/whats-app/whats-app.component.ts
+++ libs/whats-app/src/lib/whats-app/whats-app.component.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-inferrable-types */
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'monorepo-v-whats-app',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./whats-app.component.scss'],
 })
 export class WhatsAppComponent {
+  @Input() cellPhoneNumber: number = 5951137646;
+  @Input() text: string = "text=Solicitud%20de%20información";
   image_path: string = 'assets/img/whats-app.png';
+
+  /**
+   * Función para habrir ventana de whatsApp
+   * 
+   * @param urlWhatsApp 
+   * @returns void
+   */
+  openWindow(urlWhatsApp:string): void {
+    const url = `${urlWhatsApp}/${this.cellPhoneNumber}?${this.text}`;
+    window.open(url, '_blank');
+  }
+
 }
