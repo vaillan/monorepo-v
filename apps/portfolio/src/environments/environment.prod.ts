3a97b58bb7f42a8004a4ba3c0fb2bf5934aa75de
valentin-ortiz <vortiz@capi.lat>
2023-02-11 16:17:10 -0600

    Actualización

--- apps/portfolio/src/environments/environment.prod.ts
+++ apps/portfolio/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_URL:'https://vos.alumem.com.mx',
-  BaseUrl: 'https://vos.alumem.com.mx',
+  BaseUrl: 'https://vos.alumem.com.mx/',
   auth: 'api/auth',
   api: 'api'
 };
